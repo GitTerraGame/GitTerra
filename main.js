dbcaf81f3ce6f50672c24e738de5849ce2c8532b
Alex Druk <alex.druk@gmail.com>
2021-03-29 10:24:23 +0200

    generate map per repository: issue4

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 import getStdin from "get-stdin";
 import fs from "fs";
 import yargs from "yargs";
+import mkdirp from "mkdirp";
 import { spawn } from "child_process";
 
 import { generateMapHTML } from "./map.js";
 
 const min_tiles = 10;
 const colormap_file = "./color_lang.json";
-const [gitname, owner, repo] = getOwnerRepo();
-
+let [gitname, owner, repo] = getOwnerRepo();
 async function main() {
   try {
     const repoData = await readSCC();
@@ -27,10 +27,12 @@ async function main() {
         min_tiles
     );
 
+    const dirname = "./repos/" + gitname + "/" + owner + "/" + repo;
+    const filename = dirname + "/index.html";
+    mkdirp.sync(dirname);
     const mapHTML = generateMapHTML(number_of_blocks);
-
-    fs.writeFileSync("./map.html", mapHTML);
-    const subprocess = spawn("open", ["map.html"], {
+    fs.writeFileSync(filename, mapHTML); // alwais overwrite the filename
+    const subprocess = spawn("open", [filename], {
       detached: true,
       stdio: "ignore",
     });
